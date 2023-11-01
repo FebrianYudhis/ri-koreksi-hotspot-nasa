9dbaa15e8617bcdbe00b59ab70e51b318a2f8500
FebrianYudhis <killersvteam@gmail.com>
2023-11-01 15:21:02 +0700

    Versi 1.1.0

--- hotspot.php
+++ hotspot.php
@@ -1,11 +1,12 @@
 <?php
 $dataJSON = file_get_contents('php://input');
 $data = json_decode($dataJSON);
-if ($data) {
-    $namaFile = 'HotspotKoreksi.json';
+$namaFile = 'HotspotKoreksi.json';
 
+if ($data) {
     $hasilJSON = json_encode($data);
     file_put_contents($namaFile, $hasilJSON);
 } else {
+    file_put_contents($namaFile, '');
     echo "Data Tidak Valid Atau Tidak Ada Data Yang Dikirimkan.";
 }
